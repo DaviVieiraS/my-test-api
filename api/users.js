a285d3f45286a7c07e16fab3fd6adfe30630694f
DaviVieiraS <davi.vieira@estudante.ufjf.br>
2025-09-19 00:49:08 -0300

    Remove User-Agent header requirement for BG95 compatibility

--- api/users.js
+++ api/users.js
@@ -15,7 +15,7 @@ export default function handler(request, response) {
   // Enable CORS
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, User-Agent');
   
   if (request.method === 'OPTIONS') {
     response.status(200).end();
